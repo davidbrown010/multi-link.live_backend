f0845890191290bab152c486bd420a31d9e1cb79
David Brown <davidbrown@Davids-MacBook-Pro.local>
2022-01-13 08:09:36 -0600

    redirect fixes

--- routes/auth.js
+++ routes/auth.js
@@ -4,12 +4,16 @@ require('dotenv/config')
 
 router.get('/auth-redirect', async (req,res)=> {
     try {
+        console.log(req.query.redirect)
+        if (req.query.redirect == null) throw new Error('noRedirectFound')
         res.status(200).json({ redirectLink: "https://api.planningcenteronline.com/oauth/authorize?client_id="+process.env.CLIENT_ID+"&redirect_uri="+req.query.redirect+"&response_type=code&scope=services"})
     } catch (err) { res.status(500).json( { message: err } )}
 })
 
 router.get('/login-redirect', async (req,res)=> {
     try{
+        console.log(req.query.redirect)
+        if (req.query.redirect == null) throw new Error('noRedirectFound')
         res.status(200).json({ redirectLink: "https://api.planningcenteronline.com/oauth/authorize?client_id="+process.env.CLIENT_ID+"&redirect_uri="+req.query.redirect+"&response_type=code&scope=services"})
     } catch (err) { res.status(500).json( { message: err } )}
 })
