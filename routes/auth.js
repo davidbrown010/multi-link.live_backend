387ecb08ac9530a713ef8691cefb3b9fb08c3298
David Brown <davidbrown@Davids-MacBook-Pro.local>
2022-01-13 08:29:25 -0600

    protocal header test

--- routes/auth.js
+++ routes/auth.js
@@ -13,6 +13,7 @@ router.get('/auth-redirect', async (req,res)=> {
 router.get('/login-redirect', async (req,res)=> {
     try{
         console.log(req.query.redirect)
+        res.json({redirect: req.query.redirect})
         if (req.query.redirect == null) throw new Error('noRedirectFound')
         res.status(200).json({ redirectLink: "https://api.planningcenteronline.com/oauth/authorize?client_id="+process.env.CLIENT_ID+"&redirect_uri="+req.query.redirect+"&response_type=code&scope=services"})
     } catch (err) { res.status(500).json( { message: err } )}
