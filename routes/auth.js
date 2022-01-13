517c25d40a992942029b97816224796eb78cf80c
David Brown <davidbrown@Davids-MacBook-Pro.local>
2022-01-12 20:59:38 -0600

    added dynamic redirects

--- routes/auth.js
+++ routes/auth.js
@@ -4,13 +4,13 @@ require('dotenv/config')
 
 router.get('/auth-redirect', async (req,res)=> {
     try {
-        res.status(200).json({ redirectLink: "https://api.planningcenteronline.com/oauth/authorize?client_id="+process.env.CLIENT_ID+"&redirect_uri=http://192.168.4.40:5000/multi-link.live/auth/register-redirect.php&response_type=code&scope=services"})
+        res.status(200).json({ redirectLink: "https://api.planningcenteronline.com/oauth/authorize?client_id="+process.env.CLIENT_ID+"&redirect_uri="+req.query.redirect+"&response_type=code&scope=services"})
     } catch (err) { res.status(500).json( { message: err } )}
 })
 
 router.get('/login-redirect', async (req,res)=> {
     try{
-        res.status(200).json({ redirectLink: "https://api.planningcenteronline.com/oauth/authorize?client_id="+process.env.CLIENT_ID+"&redirect_uri=http://192.168.4.40:5000/multi-link.live/auth/login-redirect.php&response_type=code&scope=services"})
+        res.status(200).json({ redirectLink: "https://api.planningcenteronline.com/oauth/authorize?client_id="+process.env.CLIENT_ID+"&redirect_uri="+req.query.redirect+"&response_type=code&scope=services"})
     } catch (err) { res.status(500).json( { message: err } )}
 })
 
